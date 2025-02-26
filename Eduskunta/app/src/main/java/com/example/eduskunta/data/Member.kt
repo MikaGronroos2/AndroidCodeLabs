29172f19f48a9a706abdfe26ff327137045f6588
Mika <Mika.Gronroos2@metropolia.fi>
2025-02-26 11:51:37 +0200

    Yupdate

---
+++ Eduskunta/app/src/main/java/com/example/eduskunta/data/Member.kt
@@ -0,0 +1,22 @@
+package com.example.eduskunta
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.File
+
+data class Member(
+    val hetekaId: Int,
+    val seatNumber: Int,
+    val lastName: String,
+    val firstName: String,
+    val party: String,
+    val minister: Boolean,
+    val pictureUrl: String
+)
+
+fun readJsonFile(filePath: String): List<Member> {
+    val jsonFile = File(filePath).readText()
+    val gson = Gson()
+    val memberListType = object : TypeToken<List<Member>>() {}.type
+    return gson.fromJson(jsonFile, memberListType)
+}
